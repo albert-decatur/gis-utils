96f1527129729f5f140e4191513798ef4fd6c018
Albert Decatur <adecatur@aiddata.org>
2014-05-28 13:51:31 -0400

    add csv2shp

---
+++ csv2shp.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+# csv2shp.sh
+
+usage()
+{
+cat << EOF
+usage: $0 [OPTIONS]
+
+Export CSV as shapefile using OGR's virtual format.
+Output shapefile must not exist.
+If no -s flag is used, name of CSV will be used as name of shapefile.
+Example: $0 -c foo.csv -x longitude -y latitude -s bar.shp
+
+OPTIONS:
+   -h      Show this message
+   -c      input CSV
+   -x      CSV longitude field name
+   -y      CSV latitude field name
+   -s      output shapefile name
+EOF
+}
+
+while getopts "hx:y:c:s:" OPTION
+do
+     case $OPTION in
+         h)
+             usage
+             exit 1
+             ;;
+         c)
+             incsv=$OPTARG
+             ;;
+         x)
+             longitude=$OPTARG
+             ;;
+         y)
+             latitude=$OPTARG
+             ;;
+         s)
+             outshp=$OPTARG
+             ;;
+         ?)
+             usage
+             exit
+             ;;
+     esac
+done
+
+echo "<OGRVRTDataSource>                                                              
+        <OGRVRTLayer name=\"$(basename $incsv .csv)\">                                             
+                <SrcDataSource>$incsv</SrcDataSource>                       
+                <SrcLayer>$(basename $incsv .csv)</SrcLayer>                                     
+                <GeometryType>wkbPoint</GeometryType>                           
+                <LayerSRS>WGS84</LayerSRS>                                      
+                <GeometryField encoding=\"PointFromColumns\" x=\"$longitude\" y=\"$latitude\" />
+        </OGRVRTLayer>                                                          
+</OGRVRTDataSource>" > /tmp/$(basename $incsv .csv).vrt
+
+
+if [ -z $outshp ]; then
+	ogr2ogr $(basename $incsv .csv).shp /tmp/$(basename $incsv .csv).vrt
+else
+	ogr2ogr $outshp /tmp/$(basename $incsv .csv).vrt
+fi
+
+exit 0
