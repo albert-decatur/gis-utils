6b351be91a0ec6c65dfda2eaf9e364a67333053c
Albert Decatur <adecatur@aiddata.org>
2014-08-06 15:55:27 -0400

    add script to convert DBF to TSV

---
+++ dbf2tsv.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# make TSV from DBF
+# uses dbfdump from shapelib
+# user args: 1) input DBF
+
+indbf=$1
+header=$(
+	dbfdump --info $indbf |\
+	sed '1,9d' |\
+	awk '{print $2}' |\
+	tr '\n' '\t'
+)
+
+echo "$header"
+dbfdump -fs '	' $indbf
