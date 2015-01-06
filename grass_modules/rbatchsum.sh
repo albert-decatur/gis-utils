469b63166c589e8719c27d44d7662105486d166e
Albert Decatur <adecatur@aiddata.org>
2015-01-06 16:45:58 -0500

    resolve conflicts

--- grass_modules/rbatchsum.sh
+++ grass_modules/rbatchsum.sh
@@ -33,15 +33,12 @@
 #% description: count of rasters to sum at once
 #% required : yes
 #%End
-<<<<<<< HEAD
 #%Option
 #% key: output
 #% type: string
 #% description: path to output raster
 #% required : yes
 #%End
-=======
->>>>>>> 37931014e1427f2f76537347902687fe65a0406f
 
 if [ -z "$GISBASE" ] ; then
     echo "You must be in GRASS GIS to run this program." 1>&2
@@ -55,10 +52,7 @@ fi
 absrast=$(readlink -f $GIS_OPT_INRASTDIR)
 regionrast=$(readlink -f $GIS_OPT_REGIONRAST)
 batchsize=$(readlink -f $GIS_OPT_BATCHSIZE)
-<<<<<<< HEAD
 output=$(readlink -f $GIS_OPT_OUTPUT)
-=======
->>>>>>> 37931014e1427f2f76537347902687fe65a0406f
 
 function importrast {
 	# ingest dir of rasters
@@ -80,20 +74,6 @@ function rm_mask {
 	r.mask -r
 }
 
-<<<<<<< HEAD
-=======
-function setnull_zero {
-	# set all null values to zero
-	g.list type=rast |\
-	sed '1,2d;$d'|\
-	tr ' ' '\n'|\
-	grep -vE "^$"|\
-	parallel --gnu '
-		r.null map={} null=0
-	'
-}	
-
->>>>>>> 37931014e1427f2f76537347902687fe65a0406f
 function sum {
 	n=$( basename $batchsize )
 	# rm rasts produced by summing iterations
@@ -102,13 +82,9 @@ function sum {
 	cols=$( seq 1 $n | sed 's:^:$:g'|tr '\n' ','| sed 's:,$::g' )
 	function nextn {  echo $rasts | awk "{OFS=\"\n\";print $cols}" ;}
 	function chop { echo "$rasts" | sed "1,${n}d";}
-<<<<<<< HEAD
 	rastcount=$( echo "$rasts" | wc -l )
 	# if modulo ( raster count / batch size ) is > 0, then round up to next int
 	count_iterations=$( echo "if( $rastcount%$n > 0 ) { scale=0; $rastcount/$n + 1 } else { $rastcount/$n }" | bc )
-=======
-	count_iterations=$( echo "$( echo "$rasts" | wc -l )/$n + 1" | bc )
->>>>>>> 37931014e1427f2f76537347902687fe65a0406f
 	seq 1 $count_iterations |\
 	while read i
 	do 
@@ -122,25 +98,16 @@ function sum {
 			# version using C=A + if(isnull(B),0,B). if DN would be null make it 0
 			nextrasts=$( echo $nextrasts | tr '+' '\n' | while read rast; do echo $rast | sed "s:^:if(isnull(:g;s:$:),0,$rast):g"; done | tr '\n' '+' | sed 's:+$::g' )
 		fi
-<<<<<<< HEAD
 		echo "r.mapcalc r${i}=$nextrasts"
 		r.mapcalc r${i}=$nextrasts
 		# rm intermediate rasts
 		if [[ $i != 1 ]]; then 
 			g.remove -f rast=r$( expr $i - 1 )
 		fi
-=======
-		r.mapcalc r${i}=$nextrasts
-# tmp - dont rm intermediate rasts
-#		if [[ $i != 1 ]]; then 
-#			g.remove -f rast=r$( expr $i - 1 )
-#		fi
->>>>>>> 37931014e1427f2f76537347902687fe65a0406f
 		rasts=$(chop)
 	done
 }
 
-<<<<<<< HEAD
 function export_rast {
 	last_iteration=$( g.mlist type=rast pattern=r[0-9]+ | sed -n '$p' )
 	r.out.gdal input=$last_iteration output=$output nodata=0 format=GTiff createopt="COMPRESS=DEFLATE"
@@ -151,12 +118,5 @@ setregion_global
 rm_mask
 sum
 export_rast
-=======
-importrast
-setregion_global
-rm_mask
-#setnull_zero
-sum
->>>>>>> 37931014e1427f2f76537347902687fe65a0406f
 
 exit 0
