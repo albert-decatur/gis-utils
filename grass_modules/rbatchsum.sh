3bfc31990697fba5eee4e5859d22e735112e4f63
Albert Decatur <adecatur@aiddata.org>
2015-01-06 16:41:44 -0500

    correct batch size - used to skip rasters if modulo > 0

--- grass_modules/rbatchsum.sh
+++ grass_modules/rbatchsum.sh
@@ -3,6 +3,7 @@
 # sum a directory of tifs
 
 # NB: 
+# for now, region rast must be tif
 # mapset should not contain any rasters because all will be used to sum
 # all null values will be set to zero.  need to change this for rasters for which this is not appropriate
 
@@ -23,13 +24,19 @@
 #%Option
 #% key: regionrast
 #% type: string
-#% description: raster found in the input directory used to set region
+#% description: tif found in the input directory used to set region
 #% required : yes
 #%End
 #%Option
 #% key: batchsize
 #% type: string
 #% description: count of rasters to sum at once
+#% required : yes
+#%End
+#%Option
+#% key: output
+#% type: string
+#% description: path to output raster
 #% required : yes
 #%End
 
@@ -45,6 +52,7 @@ fi
 absrast=$(readlink -f $GIS_OPT_INRASTDIR)
 regionrast=$(readlink -f $GIS_OPT_REGIONRAST)
 batchsize=$(readlink -f $GIS_OPT_BATCHSIZE)
+output=$(readlink -f $GIS_OPT_OUTPUT)
 
 function importrast {
 	# ingest dir of rasters
@@ -54,60 +62,61 @@ function importrast {
 	done
 }
 
+function setregion_global {
+	# import regionrast
+	r.in.gdal input=$regionrast output=$( basename $regionrast .tif )
+	# NB: need to use basename b/c grass tries to expand to full "path"
+	g.region rast=$( basename $regionrast .tif)
+}
+
 function rm_mask {
 	# remove any mask that might be present
 	r.mask -r
 }
 
-
-function setregion_global {
-	# NB: need to use basename b/c grass tries to expand to full "path"
-	g.region rast=$( basename $regionrast )
-}
-
-function setnull_zero {
-	# set all null values to zero
-	g.list type=rast |\
-	sed '1,2d;$d'|\
-	tr ' ' '\n'|\
-	grep -vE "^$"|\
-	parallel --gnu '
-		r.null map={} null=0
-	'
-}	
-
 function sum {
 	n=$( basename $batchsize )
-	rasts=$( g.list type=rast | sed '1,2d;$d'|tr ' ' '\n'|grep -vE "^$" )
+	# rm rasts produced by summing iterations
+	g.mremove -f rast=r[0-9]*
+	rasts=$( g.list type=rast | sed '1,2d;$d'|tr ' ' '\n'|grep -vE "^$" | grep -vE "^$( basename "$regionrast" .tif )$")
 	cols=$( seq 1 $n | sed 's:^:$:g'|tr '\n' ','| sed 's:,$::g' )
 	function nextn {  echo $rasts | awk "{OFS=\"\n\";print $cols}" ;}
 	function chop { echo "$rasts" | sed "1,${n}d";}
-	count_iterations=$( echo "$( echo "$rasts" | wc -l )/$n + 1" | bc )
+	rastcount=$( echo "$rasts" | wc -l )
+	# if modulo ( raster count / batch size ) is > 0, then round up to next int
+	count_iterations=$( echo "if( $rastcount%$n > 0 ) { scale=0; $rastcount/$n + 1 } else { $rastcount/$n }" | bc )
 	seq 1 $count_iterations |\
 	while read i
 	do 
-		if [[ $i != 1 ]]; then 
-			nextrasts=$( nextn | grep -vE "^$" | tr '\n' ' ' | sed "s:$:\n$( expr ${i} - 1 ):g" | tr ' ' '\n' | tr '\n' '+' | sed 's:\+$::g' | sed 's:+\+:+:g' )
+		if [[ $i -ne 1 ]]; then 
+			nextrasts=$( nextn | grep -vE "^$" | tr '\n' ' ' | sed "s:$:\nr$( expr ${i} - 1 ):g" | tr ' ' '\n' | tr '\n' '+' | sed 's:\+$::g' | sed 's:+\+:+:g' )
 			# version using C=A + if(isnull(B),0,B). if DN would be null make it 0
 			nextrasts=$( echo $nextrasts | tr '+' '\n' | while read rast; do echo $rast | sed "s:^:if(isnull(:g;s:$:),0,$rast):g"; done | tr '\n' '+' | sed 's:+$::g' )
+			
 		else 
 			nextrasts=$( nextn | grep -vE "^$" |tr '\n' '+' | sed 's:\+$::g')
 			# version using C=A + if(isnull(B),0,B). if DN would be null make it 0
 			nextrasts=$( echo $nextrasts | tr '+' '\n' | while read rast; do echo $rast | sed "s:^:if(isnull(:g;s:$:),0,$rast):g"; done | tr '\n' '+' | sed 's:+$::g' )
 		fi
+		echo "r.mapcalc r${i}=$nextrasts"
 		r.mapcalc r${i}=$nextrasts
-# tmp - dont rm intermediate rasts
-#		if [[ $i != 1 ]]; then 
-#			g.remove -f rast=r$( expr $i - 1 )
-#		fi
+		# rm intermediate rasts
+		if [[ $i != 1 ]]; then 
+			g.remove -f rast=r$( expr $i - 1 )
+		fi
 		rasts=$(chop)
 	done
 }
 
-#importrast
-#rm_mask
-#setregion_global
-#setnull_zero
+function export_rast {
+	last_iteration=$( g.mlist type=rast pattern=r[0-9]+ | sed -n '$p' )
+	r.out.gdal input=$last_iteration output=$output nodata=0 format=GTiff createopt="COMPRESS=DEFLATE"
+}
+
+importrast
+setregion_global
+rm_mask
 sum
+export_rast
 
 exit 0
