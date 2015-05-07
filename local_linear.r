89f3e9bcf478daf55c486459a2df6934b663f2b2
Albert Decatur <adecatur@aiddata.org>
2015-05-07 16:37:15 -0400

    add script for R akima local linear interpolation TODO add akima cubic spline script to allow for extrapolation

--- local_linear.r
+++ local_linear.r
@@ -1,15 +1,13 @@
-# get *n* degree polynomial trend surface from point shapefile
 # originally made to produce maps of Saffir-Simpson index for all Atlantic hurricanes in HURDAT
 # TODO: add basemap to png
-# TODO: choose best polynomial based on data
 # TODO: export tif
-# args are: 1) input GIS vector points, 2) polynomial degree, 3) trmat grid dimensions (n X n)
-# example use: Rscript --vanilla $0 AL032009.shp 1 500
+# args are: 1) input GIS vector points
+# example use: Rscript --vanilla $0 AL132003.shp
 
 # load prereqs
 library(methods) # this is only required when running with Rscript
 library(maptools)
-library(spatial)
+library(akima)
 # get user args
 args <- commandArgs(trailingOnly = TRUE)
 # load points
@@ -22,19 +20,19 @@ layerName <- unlist(strsplit(basename(args[1]),"\\."))[1]
 d$lon <- as.numeric(as.character(d$lon))
 d$lat <- as.numeric(as.character(d$lat))
 d$sshws <- as.numeric(as.character(d$sshws))
-# make trend surface
-t <- surf.ls(as.numeric(args[2]),d$lon,d$lat,d$sshws)
-# make trend surface over grid
-g <- trmat(t,bbox(d)[1,1],bbox(d)[1,2],bbox(d)[2,1],bbox(d)[2,2],as.numeric(args[3]))
+# linear interpolation using akima package
+# note that map units are used as the spatial res, and that BBOX is used for grid
+# TODO: user arg for spatial res
+d.li <- interp(d$lon,d$lat,d$sshws, xo=seq(bbox(d)[1,1],bbox(d)[1,2], length=abs(bbox(d)[1,1] - bbox(d)[1,2])),yo=seq(bbox(d)[2,1],bbox(d)[2,2], length=abs(bbox(d)[2,1] - bbox(d)[2,2])))
 # enforce min (0) and max (particular to the HURDAT points)
 # first find the SSHWS max
 maxsshws <- max(d$sshws)
 # now enforce 0 min and SSHWS max on trend grid
-g$z <- round(replace(replace(g$z,g$z>maxsshws,maxsshws),g$z<0,0))
+maxsshws <- max(d$sshws); d.li$z <- round(replace(replace(d.li$z,d.li$z>maxsshws,maxsshws),d.li$z<0,0))
 # visual
 png(paste("/tmp/",layerName,".png",sep=''))
-image(g)
-title(main=paste(layerName, ", ",args[2]," Degree Polynomial",sep=''))
-contour(g,labcex=1,add=T,nlevels=maxsshws)
+image(d.li)
+title(main=paste(layerName, ", "," Linear Interpolation from Akima",sep=''))
+contour(d.li,labcex=1,add=T,nlevels=maxsshws)
 points(d$lon,d$lat)
 dev.off()
